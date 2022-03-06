8cc46312810d09cd235c6be3f4dee4851a080c38
akash <ak.akashpatil5@gmail.com>
2022-03-06 11:47:54 +0530

    test-6

---
+++ Test-6/index.js
@@ -0,0 +1,17 @@
+
+        function iphone() {
+            document.getElementById('name').innerHTML = 'iPhone 13 Pro Max '
+            document.getElementById('info').innerHTML = 'iPhone 13 Pro Max Oh. So. Even more Pro. iPhone 13 Pro Max. A total Pro. The biggest Pro camera system upgrade ever. Super Retina XDR display with ProMotion for a faster, more responsive feel.'
+            document.getElementById('price').innerHTML = '₹1,29,000 '
+        }
+
+        function oppo() {
+            document.getElementById('name1').innerHTML = 'OPPO Reno7 Pro '
+            document.getElementById('info1').innerHTML = '6.43" inch (16.3cm) FHD+ Super AMOLED Punch-hole Display with 2400x1080 pixels. Larger Screen to Body ratio of 90.8%, In-display Fingerprint 3.0'
+            document.getElementById('price1').innerHTML = '₹39,990 '
+        }
+        function vivo() {
+            document.getElementById('name2').innerHTML = 'Vivo V23 Pro '
+            document.getElementById('info2').innerHTML = 'Grab Vivo V23 Pro with stunning camera specifications and MediaTek Dimensity 1200 Processor. Grab online from Poorvika at the best price.'
+            document.getElementById('price2').innerHTML = '₹39,000 '
+        }
